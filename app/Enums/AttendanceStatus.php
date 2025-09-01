eab025f6a7bdfae14f2ad1825110e576c4528213
mabe4242 <kouiy42@gmail.com>
2025-09-01 15:03:08 +0900

    勤怠登録画面の作成

---
+++ app/Enums/AttendanceStatus.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Enums;
+
+class AttendanceStatus
+{
+    public const OFF = 0;
+    public const WORKING = 1;
+    public const BREAK = 2;
+    public const FINISHED = 3;
+}
--- resources/views/user/attendance.blade.php
+++ resources/views/user/attendance.blade.php
@@ -1,20 +1,58 @@
 @extends('layouts.app')
 
-{{-- @section('css')
+@section('css')
     <link rel="stylesheet" href="{{ asset('css/user/attendance.css') }}">
-@endsection --}}
+@endsection
 
 @section('content')
     <x-user_header></x-user_header>
-    <div class="content">
-        {{-- ここのspanもstatusによって表示を切り替える --}}
-        <span class="status">勤務外</span>
-        <p>2023年6月1日(木)</p>
-        <p>08:00</p>
-        {{-- ここの{{ route('user.attendance') }}どう書けばいいんだろう、statusによって変わるんだよな --}}
-        <form action="" method="post">
-            @csrf
-            <button type="submit" class="attendance_btn">出勤</button>
-        </form>
+    <div class="content__wrapper">
+        <div class="content">
+            @php
+                use App\Enums\AttendanceStatus;
+            @endphp
+            
+
+            {{-- ここのspanもstatusによって表示を切り替える --}}
+
+            {{-- <span class="status">勤務外</span>
+            <p class="attendance-date">2023年6月1日(木)</p>
+            <p class="attendance-time">08:00</p>
+            <form action="" method="post">
+                @csrf
+                <button type="submit" class="attendance_btn">出勤</button>
+            </form> --}}
+
+
+            {{-- <span class="status">出勤中</span>
+            <p class="attendance-date">2023年6月1日(木)</p>
+            <p class="attendance-time">08:00</p>
+            <div class="btn__area">
+                <form action="" method="post">
+                    @csrf
+                    <button type="submit" class="attendance_btn">退勤</button>
+                </form>
+                <form action="" method="post">
+                    @csrf
+                    <button type="submit" class="break_btn">休憩入</button>
+                </form>
+            </div> --}}
+
+
+            {{-- <span class="status">休憩中</span>
+            <p class="attendance-date">2023年6月1日(木)</p>
+            <p class="attendance-time">08:00</p>
+            <form action="" method="post">
+                @csrf
+                <button type="submit" class="break_btn">休憩戻</button>
+            </form> --}}
+
+
+            <span class="status">退勤済</span>
+            <p class="attendance-date">2023年6月1日(木)</p>
+            <p class="attendance-time">08:00</p>
+            <p class="message">お疲れ様でした。</p>
+
+        </div>
     </div>
 @endsection
