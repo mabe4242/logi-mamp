82be0d49f7446a5a0a1a73808b0a81eeb1326ff1
mabe4242 <kouiy42@gmail.com>
2025-09-16 21:02:43 +0900

    管理者ヘッダーの作成

--- resources/views/components/admin_header.blade.php
+++ resources/views/components/admin_header.blade.php
@@ -1,28 +1,28 @@
-<header class="header">
+<header class="header header__admin">
     <div class="header__inner">
         <div class="header-utilities">
             <a class="header__logo" href="/admin/attendance/list">
                 <img src="{{ asset('icons/logo.png') }}" alt="COACHTECHのロゴ" class="logo-img">
             </a>
-            <nav>
+            <nav class="nav__admin">
                 <ul class="header-nav">
                     @if (Auth::check())
                         <li class="header-nav__item">
-                            <form class="form" action="/admin/logout" method="post">
+                            <a class="header-nav__link btn" href="{{ route('admin.attendance.index') }}">勤怠一覧</a>
+                        </li>
+                        <li class="header-nav__item">
+                            <a class="header-nav__link btn" href="{{ route('admin.staff.index') }}">スタッフ一覧</a>
+                        </li>
+                        <li class="header-nav__item">
+                            <a class="header-nav__link btn" href="{{ route('admin.attendance_requests.index') }}">申請一覧</a>
+                        </li>
+                        <li class="header-nav__item">
+                            <form class="form" action="{{ route('admin.logout') }}" method="post">
                                 @csrf
                                 <button class="header-nav__button btn">ログアウト</button>
                             </form>
                         </li>
-                    @else 
-                        <li class="header-nav__item">
-                            <form class="form" action="/admin/login">
-                                <button class="header-nav__button btn">ログイン</button>
-                            </form>
-                        </li>
                     @endif
-                    <li class="header-nav__item">
-                        <a class="header-nav__link btn" href="">マイページ</a>
-                    </li>
                 </ul>
             </nav>
         </div>
